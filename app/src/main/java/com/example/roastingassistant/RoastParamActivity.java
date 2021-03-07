719b55fa5588442dda7b131c17954da126a4398c
robert <robertnmoffat@gmail.com>
2021-03-06 20:52:41 -0800

    Finished roast param UI, and fixed lower add panel in roast, bean, and blend activities.

--- app/src/main/java/com/example/roastingassistant/RoastParamActivity.java
+++ app/src/main/java/com/example/roastingassistant/RoastParamActivity.java
@@ -3,10 +3,16 @@ package com.example.roastingassistant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class RoastParamActivity extends AppCompatActivity {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoastParamActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +20,30 @@ public class RoastParamActivity extends AppCompatActivity {
         setContentView(R.layout.activity_roast_param);
 
         Spinner spinner = findViewById(R.id.bean_spinner);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.bean_array, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        setupSpinner(spinner);
+
+    }
+
+    public void setupSpinner(Spinner spinner){
+        //spinner click listener
+        spinner.setOnItemSelectedListener(this);
+        //set spinner drop down elements
+        List<String> categories = new ArrayList<String>();
+        categories.add("Beans");//TODO: add stuff from database later
+        //adaptor for categories and spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.spinner_textview, categories);
+        //drop down layout style
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
